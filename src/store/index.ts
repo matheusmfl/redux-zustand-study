16e1ab8af4c03fd2471e06693c35bf9cb9c01d65
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-06-30 17:31:30 -0300

    Update project, create videoPlayer components

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,9 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 import { useSelector, TypedUseSelectorHook } from 'react-redux'
 
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['fazer café', 'estudar reduce', 'Estudar mais'],
-  reducers: {
-    add: (state, action) => {
-      console.log(action)
-      state.push(action.payload)
-    },
-  },
-})
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 })
-
-export const { add } = todoSlice.actions
 
 export type RootState = ReturnType<typeof store.getState>
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
