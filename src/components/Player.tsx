6d7f35b7da17366440e4257ac20bf04221b36b0a
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-01 16:03:03 -0300

    Config AutoPlay and NextFunction Statement

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { useAppSelector } from '@/store'
+import { next } from '@/store/slices/player'
 import ReactPlayer from 'react-player'
+import { useDispatch } from 'react-redux'
 
 export function Player() {
+  const dispatch = useDispatch()
+
   const lesson = useAppSelector((state) => {
     const { currentModuleIndex, currentLessonIndex } = state.player
+
+
 
     const currentLesson =
       state.player.course.modules[currentModuleIndex].lessons[
@@ -14,12 +20,17 @@ export function Player() {
     return currentLesson
   })
 
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
+        onEnded={handlePlayNext}
         url={`https://youtube.com/watch?v=${lesson.id}`}
       />
     </div>
